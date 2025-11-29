36b99d4e2f53e31199cb320ee73590bd7a684a8b
Locch65 <locch@libero.it>
2025-11-29 01:25:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // =====================
 // VERSIONE SCRIPT
 // =====================
-const SCRIPT_VERSION = "1.0.14";  // Aggiorna questo numero ad ogni modifica
+const SCRIPT_VERSION = "1.0.15";  // Aggiorna questo numero ad ogni modifica
 
 document.addEventListener("DOMContentLoaded", () => {
   // Mostra la versione nello UI
@@ -576,6 +576,7 @@ function init() {
 }
 
 document.addEventListener("DOMContentLoaded", init);
+
 
 
 
