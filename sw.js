1cc3c9779a91ba4a65c7b71ee78f03c8ff6b7985
Locch65 <locch@libero.it>
2025-12-16 16:28:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,56 @@
+// -----------------------------------------------------------------------------------
+// TODO List:
+//
+//  Calendario.html
+//    - Aggiungere in hamburger le voci: "Anagrafica", "Classifiche", "Impostazioni", 
+//    - abilitare update automatico punteggi solo se c'è una partita live
+//
+//  Match.html
+//    - sistemare i font su browser, sono troppo grandi. i bottoni si sovrappongoni
+//    - leggere lista giocatori da Google Sheet
+//    - inserire hamburger menu con le voci: "Go Live", "Salva", "Annulla", "Anagrafica", "Calendario", "Classifiche", "Impostazioni", 
+//    - bottone "Video" cambia colore e testo (in "Live") se c'e' una live attiva. inoltre, ogni volta che fa il refresh della pagina verifica l'esistenza del video per mostrare il bottone
+//    - quando non esistono punteggi per un una partita, creare un giocare fittizio "Polismile A", e assegnare tutti i punti a lui.
+//    - password Admin da crittografare
+//    - implementare dark mode
+//
+//  DirettaVideo.html
+//    - inserire punteggio e ultimo marcatore in sovraimpressione (quando in orizzontale)
+//    - inserire lista giocatori con relativi punti e punteggio partita sotto al video (quando in verticale)
+//
+//  Angrafica.html
+//    - 1) mostrare lista Roster. Quando si clicca su un nome
+//         1.1) si apre dettaglio anagrafica e statistiche    
+//
+//
+//  Statistiche.html
+//    - mostrare tabella con "#, Nome, Punti Totali, Punti da 1, da 2, da 3, Media per Partita, #Partite"
+//    - le intestazioni delle colonne della tabella sono bottoni che permettono il sort
+//
+//
+//  Impostazioni. html
+//    - pagina dedicata per abilitare/disabilitare, settare:
+//       - Dark Mode (toggle)
+//       - refresh rate dei punteggi live (di default 5 secondi)
+//
+//
+//
+// -----------------------------------------------------------------------------------
+
+
 // =====================
 // VERSIONE SERVICE WORKER
 // =====================
-const SW_VERSION = "1.0.58"; // incrementa sempre ad ogni release
+const SW_VERSION = "1.0.59"; // incrementa sempre ad ogni release
 const CACHE_NAME = "basket-app-cache-" + SW_VERSION;
 
 // Usa percorsi RELATIVI per GitHub Pages (niente "/" iniziale)
 const FILES_TO_CACHE = [
   "./index.html",
-  "./style.css",
-  "./script.js?v=" + SW_VERSION,
+  "./calendario.css",
+  "./match.css",
+  "./calendaio.js?v=" + SW_VERSION,
+  "./match.js?v=" + SW_VERSION,
   "./manifest.json"
 ];
 
