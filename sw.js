0609a471d9d0d83ac71a54216d975b7a4cf2b968
Locch65 <locch@libero.it>
2025-12-19 18:43:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -59,7 +59,7 @@
 // =====================
 // VERSIONE SERVICE WORKER
 // =====================
-const SW_VERSION = "1.0.64"; // incrementa sempre ad ogni release
+const SW_VERSION = "1.0.65"; // incrementa sempre ad ogni release
 const CACHE_NAME = "basket-app-cache-" + SW_VERSION;
 
 // Usa percorsi RELATIVI per GitHub Pages (niente "/" iniziale)
@@ -130,3 +130,4 @@ self.addEventListener("fetch", (event) => {
 self.addEventListener("message", (event) => {
   // placeholder se vuoi gestire comandi dal client
 });
+
